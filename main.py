1658a85b8c25ecc72b9d80931460d7be02ae516d
Clara <clara.twq@gmail.com>
2026-04-23 16:34:30 +0800

    port

--- main.py
+++ main.py
@@ -368,5 +368,5 @@ def index():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 7860))
-    app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT"))
+    app.run(host="0.0.0.0", port=port, debug=False)
